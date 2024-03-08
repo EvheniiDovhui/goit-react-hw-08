f6be9ec56cbd1201fc4486231a271675c1f8c487
EvheniiDovhui <edovguy97@gmail.com>
2024-03-08 16:10:08 +0200

    Formik + yup

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,45 +1,64 @@
-import toast from 'react-hot-toast'
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { useDispatch } from 'react-redux'
 import { logIn } from '../../redux/auth/operations'
+import { toast } from 'react-hot-toast'
 import css from './LoginForm.module.css'
 import Button from '@mui/material/Button'
 
 const LoginForm = () => {
 	const dispatch = useDispatch()
 
-	const handleSubmit = e => {
-		e.preventDefault()
-		const form = e.currentTarget
-		dispatch(
-			logIn({
-				email: form.elements.email.value,
-				password: form.elements.password.value,
-			})
-		)
+	const initialValues = {
+		email: '',
+		password: '',
+	}
+
+	const onSubmit = (values, { resetForm }) => {
+		dispatch(logIn(values))
 			.unwrap()
 			.then(() => {
 				toast.success('login success')
+				resetForm()
 			})
 			.catch(() => {
 				toast.error('login error')
 			})
-		form.reset()
+	}
+
+	const validate = values => {
+		const errors = {}
+		if (!values.email) {
+			errors.email = 'Email is required'
+		}
+		if (!values.password) {
+			errors.password = 'Password is required'
+		}
+		return errors
 	}
 
 	return (
-		<form className={css.form} onSubmit={handleSubmit} autoComplete='off'>
-			<label className={css.label}>
-				Email
-				<input type='email' name='email' />
-			</label>
-			<label className={css.label}>
-				Password
-				<input type='password' name='password' />
-			</label>
-			<Button variant='contained' className={css.btn} type='submit'>
-				Log In
-			</Button>
-		</form>
+		<Formik
+			initialValues={initialValues}
+			onSubmit={onSubmit}
+			validate={validate}
+		>
+			<Form className={css.form} autoComplete='off'>
+				<div className={css.label}>
+					<label htmlFor='email'>Email</label>
+					<Field type='email' id='email' name='email' />
+					<ErrorMessage name='email' component='div' className={css.error} />
+				</div>
+				<div className={css.label}>
+					<label htmlFor='password'>Password</label>
+					<Field type='password' id='password' name='password' />
+					<ErrorMessage name='password' component='div' className={css.error} />
+				</div>
+				<Button variant='contained' className={css.btn} type='submit'>
+					Log In
+				</Button>
+			</Form>
+		</Formik>
 	)
 }
 
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,42 +1,60 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { useDispatch } from 'react-redux'
 import { register } from '../../redux/auth/operations'
 import css from './RegisterForm.module.css'
 import Button from '@mui/material/Button'
+import * as Yup from 'yup'
 
 const RegisterForm = () => {
 	const dispatch = useDispatch()
 
-	const handleSubmit = e => {
-		e.preventDefault()
-		const form = e.currentTarget
-		dispatch(
-			register({
-				name: form.elements.name.value,
-				email: form.elements.email.value,
-				password: form.elements.password.value,
-			})
-		)
-		form.reset()
+	const initialValues = {
+		name: '',
+		email: '',
+		password: '',
+	}
+
+	const validationSchema = Yup.object().shape({
+		name: Yup.string().required('Username is required'),
+		email: Yup.string().email('Invalid email').required('Email is required'),
+		password: Yup.string()
+			.min(7, 'Password must be at least 7 characters')
+			.required('Password is required'),
+	})
+
+	const onSubmit = (values, { resetForm }) => {
+		dispatch(register(values))
+		resetForm()
 	}
 
 	return (
-		<form className={css.form} onSubmit={handleSubmit} autoComplete='off'>
-			<label className={css.label}>
-				Username
-				<input type='text' name='name' />
-			</label>
-			<label className={css.label}>
-				Email
-				<input type='email' name='email' />
-			</label>
-			<label className={css.label}>
-				Password
-				<input type='password' name='password' />
-			</label>
-			<Button variant='contained' className={css.btn} type='submit'>
-				Register
-			</Button>
-		</form>
+		<Formik
+			initialValues={initialValues}
+			onSubmit={onSubmit}
+			validationSchema={validationSchema}
+		>
+			<Form className={css.form} autoComplete='off'>
+				<div className={css.label}>
+					<label htmlFor='name'>Username</label>
+					<Field type='text' id='name' name='name' />
+					<ErrorMessage name='name' component='div' className={css.error} />
+				</div>
+				<div className={css.label}>
+					<label htmlFor='email'>Email</label>
+					<Field type='email' id='email' name='email' />
+					<ErrorMessage name='email' component='div' className={css.error} />
+				</div>
+				<div className={css.label}>
+					<label htmlFor='password'>Password</label>
+					<Field type='password' id='password' name='password' />
+					<ErrorMessage name='password' component='div' className={css.error} />
+				</div>
+				<Button variant='contained' className={css.btn} type='submit'>
+					Register
+				</Button>
+			</Form>
+		</Formik>
 	)
 }
 
